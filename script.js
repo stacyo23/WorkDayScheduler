548f4423990f4471ae256c9445067d820947e35f
Stacy Betts <stacyo23@yahoo.com>
2020-10-28 22:15:34 -0500

    played with a few more variables

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ var Day = moment().format("dddd, MMMM Do");
 console.log(Day); 
 $("#currentDay").append(Day); 
 
+var time1 = beforeTime.add(0, "h");
+
+
 var currTime = moment().format("hA"); 
 console.log(currTime); 
+
+var startTime = moment().startOf("date")._d; 
+console.log(startTime); 
+
+newstartTime = moment(startTime).add("9", "hours"); 
+
+console.log(newstartTime._d); 
+
+
+
+
 
 // if (currTime.isBefore(".time-block")) {
 //     $(".timeblock").attr("class", "future"); 

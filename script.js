3b2303df6ea026fc2447d8f526c03fd894ab75b4
Stacy Betts <stacyo23@yahoo.com>
2020-10-28 21:21:16 -0500

    added time blocks and favicon

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ var Day = moment().format("dddd, MMMM Do");
 console.log(Day); 
 $("#currentDay").append(Day); 
 
+var currTime = moment().format("hA"); 
+console.log(currTime); 
+
+// if (currTime.isBefore(".time-block")) {
+//     $(".timeblock").attr("class", "future"); 
+
+// }else if (currTime.isSame(".time-block")) {
+//     $(".timeblock").attr("class", "present"); 
+
+// }else (currTime.isAfter(".timeblock")) {
+//     $(".timeblock").attr("class", "past"); 
+
+// }; 
+ 

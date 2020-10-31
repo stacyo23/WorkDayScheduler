e4635c4aabcc7a78bd97f0575d455f5e5dbd72d6
Stacy Betts <stacyo23@yahoo.com>
2020-10-30 21:31:50 -0500

    button functionality & input to localStorage

--- script.js
+++ script.js
@@ -8,49 +8,63 @@ $("#currentDay").append(day);
 
 //gives current time in hour+AM/PM format
 var currTime = moment().format("hhA"); 
-console.log(currTime); 
 
 var startTime = moment().startOf("day").add(9, "hours");
 
 //get first hour
 var time1 =startTime.add(0, "h"); 
-time1=time1.format("hA"); 
+time1=time1.format("h A "); 
 $(".block1").text(time1); 
 //get 2nd hour - error on the line
 var time2=startTime.add(1, "hour"); 
-time2=time2.format("hA");
+time2=time2.format("h A");
 $(".block2").text(time2); 
 
 var time3=startTime.add(1, "hour"); 
-time3=time3.format("hA");
+time3=time3.format("h A");
 $(".block3").text(time3); 
 
 var time4=startTime.add(1, "hour"); 
-time4=time4.format("hA");
+time4=time4.format("h A");
 $(".block4").text(time4); 
 
 
 var time5=startTime.add(1, "hour"); 
-time5=time5.format("hA");
+time5=time5.format("h A");
 $(".block5").text(time5); 
 
 var time6=startTime.add(1, "hour"); 
-time6=time6.format("hA");
+time6=time6.format("h A");
 $(".block6").text(time6); 
 
 var time7=startTime.add(1, "hour"); 
-time7=time7.format("hA");
+time7=time7.format("h A");
 $(".block7").text(time7); 
 
 var time8=startTime.add(1, "hour"); 
-time8=time8.format("hA");
+time8=time8.format("h A");
 $(".block8").text(time8); 
 
 var time9=startTime.add(1, "hour"); 
-time9=time9.format("hA");
+time9=time9.format("h A");
 $(".block9").text(time9); 
 
 
+function compareTime() {
+
+    
+}
+
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+
+    // This line grabs the input from the textbox
+    var textInput = $(this).siblings(".form-control").val().trim();
+    var hourofDay= $(this).parent().data("hour"); 
+    
+localStorage.setItem(hourofDay, textInput); 
+
+});
 });
 
 
